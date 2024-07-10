498d87f915f19bf3bf34bfb361eb509e90bb34fe
Sivapriyan7 <sivapriyan7siva@gmail.com>
2024-07-10 10:26:56 +0530

    ds

--- src/DS_Practice/DoublyLinkedList/DoublyLinkedList.java
+++ src/DS_Practice/DoublyLinkedList/DoublyLinkedList.java
@@ -2,11 +2,18 @@ package DS_Practice.DoublyLinkedList;
 
 class Node
 {
-    
+    int data;
+    Node prev;
+    Node next;
 }
 
 
 class DoublyLinkedList {
 
+
+
+    public static void main(String[] args) {
+        DoublyLinkedList DlList = new DoublyLinkedList();
+    }
 
 }
