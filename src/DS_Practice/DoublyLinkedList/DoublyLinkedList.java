08b7d1490c31bb29c420392638ad2eecdbbabcfc
Sivapriyan7 <sivapriyan7siva@gmail.com>
2024-07-10 12:17:07 +0530

    DS

--- src/DS_Practice/DoublyLinkedList/DoublyLinkedList.java
+++ src/DS_Practice/DoublyLinkedList/DoublyLinkedList.java
@@ -61,9 +61,65 @@ class DoublyLinkedList {
         System.out.print("<-->null");
     }
 
-    public void insertAt(int data)
+    public void insertAt(int index,int data)
     {
+        if (index==1)
+        {
+            addAtFirst(data);
+        }
+        else {
+            Node current = head;
+            for (int i=1; i<index-1; i++)
+            {
+                current = current.next;
+            }
+            if(current.next==null)
+            {
+                addAtLast(data);
+            }
+            else {
+                Node temp = new Node(data);
+                temp.next = current.next;
+                temp.prev = current;
+                current.next = temp;
+                current.next.prev = temp;
+            }
+        }
+    }
+    public void deleteAt(int index)
+    {
+        if(index==1)
+        {
+            deleteFirst();
+        }
 
+        Node current = head;
+        for (int i=1; i<index;i++)
+        {
+            current = current.next;
+        }
+
+        if(current.next==null)
+        {
+            deleteLast();
+        }
+        else {
+            current.prev.next = current.next;
+            current.next.prev = current.prev;
+            current.next = null;
+            current.prev = null;
+        }
+
+    }
+    public void deleteFirst()
+    {
+        head = head.next;
+        head.prev = null;
+    }
+    public void deleteLast()
+    {
+        tail = tail.prev;
+        tail.next = null;
     }
 
     public static void main(String[] args) {
@@ -73,6 +129,11 @@ class DoublyLinkedList {
         DlList.addAtFirst(13);
         DlList.addAtFirst(14);
         DlList.addAtLast(22);
+        DlList.insertAt(6,200);
+        DlList.deleteAt(5);
+//        DlList.deleteFirst();
+//        DlList.deleteFirst();
+//        DlList.deleteLast();
         DlList.show();
     }
 
