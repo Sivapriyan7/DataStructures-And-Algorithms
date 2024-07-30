c89a740b6d641bec92b37764f040ce4e142168a2
Sivapriyan7 <sivapriyan7siva@gmail.com>
2024-07-30 10:38:40 +0530

    DS

--- src/Coding_Problems/Accenture/MinValBinary.java
+++ src/Coding_Problems/Accenture/MinValBinary.java
@@ -1,2 +1,40 @@
-package Coding_Problems.Accenture;public class MinValBinary {
+package Coding_Problems.Accenture;
+
+import java.util.Scanner;
+
+public class MinValBinary {
+
+    public static int MinValBinary(int num){
+        int count = 0;
+        while (num>0)
+        {
+            int c = num&1;
+            if(c==1)
+            {
+                count++;
+            }
+            num = num>>1;
+        }
+
+        int ans = 0,power=1;
+        for (int i=0; i<count; i++)
+        {
+            ans += power*1;
+            power *= 2;
+        }
+        return ans;
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(MinValBinary(num));
+        byte x = 127;
+        x++;
+        x++;
+        System.out.println(x);
+
+    }
 }
