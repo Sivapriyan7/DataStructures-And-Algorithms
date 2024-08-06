acb0549b111a2f72ff5ef72ba364d36a4d9c08ed
Sivapriyan7 <sivapriyan7siva@gmail.com>
2024-08-06 21:08:32 +0530

    DS

--- src/Coding_Problems/PBL/Collections/Employee.java
+++ src/Coding_Problems/PBL/Collections/Employee.java
@@ -7,6 +7,7 @@ public class Employee {
     private String gender;
     private float salary;
 
+
     // Constructor
     public Employee(int empId, String empName, String email, String gender, float salary) {
         this.empId = empId;
