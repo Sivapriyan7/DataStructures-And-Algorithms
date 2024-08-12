70fc36901e7aac1df13c1975a929a42ee90ae96b
Sivapriyan7 <sivapriyan7siva@gmail.com>
2024-08-12 22:35:46 +0530

    DS

--- src/DS_Practice/Graph/FloodFill_Algorithm/FloodFill.java
+++ src/DS_Practice/Graph/FloodFill_Algorithm/FloodFill.java
@@ -5,20 +5,24 @@ class FloodFill
     public static void dfs(int row, int col,int[][] ans, int[][] image, int newColor, int[] delRow,int[] delCol,int iniColor)
     {
         ans[row][col] = newColor;
+        System.out.println("row "+row+"Col "+col);
         int n = image.length;
         int m = image[0].length;
         for(int i=0; i<4; i++)
         {
+            System.out.println("in 1");
             int nrow = row+delRow[i];
             int ncol = col+delCol[i];
-            if(nrow>0 && nrow<n && ncol>0 && ncol<m && image[nrow][ncol]==iniColor && ans[nrow][ncol]!=newColor)
+            System.out.println("nrow ");
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol]==iniColor && ans[nrow][ncol]!=newColor)
             {
+                System.out.println("in rec");
                 dfs(nrow,ncol,ans,image,newColor,delRow,delCol,iniColor);
             }
         }
     }
 
-    public int[][] floodFill(int[][] image, int sr, int sc, int newColor)
+    public static int[][] floodFill(int[][] image, int sr, int sc, int newColor)
     {
         // Code here
         int[][] ans = image;
@@ -29,5 +33,12 @@ class FloodFill
         dfs(sr,sc,ans,image,newColor,delRow,delCol,iniColor);
         return ans;
 
+    }
+
+    public static void main(String[] args) {
+        int[][] image = {{1,1,0},
+                         {1,0,1},
+                         {1,1,2}};
+        floodFill(image,1,1,2);
     }
 }
