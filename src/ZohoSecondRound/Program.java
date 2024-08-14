32f28c564539df5c933aec5d52ad5a8df80ee96f
Sivapriyan7 <sivapriyan7siva@gmail.com>
2024-08-14 23:39:39 +0530

    DS

--- src/ZohoSecondRound/Program.java
+++ src/ZohoSecondRound/Program.java
@@ -1,4 +1,24 @@
 package ZohoSecondRound;
 
 public class Program {
+
+    public static void ProgramPattern(String str)
+    {
+        int size = str.length();
+        int print=0;
+        int pointer = size/2;
+        int index = pointer+1;
+        String result="";
+        boolean flag = false;
+        for (int i=pointer; i<size; i--)
+        {
+
+            result += str.substring(pointer,index++);
+        }
+    }
+
+    public static void main(String[] args) {
+        String S1 = "PROGRAM";
+        ProgramPattern(S1);
+    }
 }
